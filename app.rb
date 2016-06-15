caaa45ee0b49b525e3433a1bffa69d7bbf18966c
Jiahao Li <isundaylee.reg@gmail.com>
2016-06-14 20:44:52 -0700

    Apparently, it's not always Q2.

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'open-uri'
 
 URL = 'http://quotes.wsj.com/%s'
-REGEX = /will report Q2 earnings on (\d\d\/\d\d\/\d\d\d\d)/
+REGEX = /will report .* earnings on (\d\d\/\d\d\/\d\d\d\d)/
 
 get '/:company' do
   company = params[:company]
